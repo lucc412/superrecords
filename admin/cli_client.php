2b9edda590a84cfcb1136b793d9483f72f278322
unknown <mailtosiddhesh@gmail.com>
2013-04-15 16:16:24 +0530

    Client filter

--- admin/cli_client.php
+++ admin/cli_client.php
@@ -8,7 +8,20 @@ include (PHPFUNCTION);
 // create class object for class function access
 $objCallData = new Practice_Class();
 
+
 if($_SESSION['validUser']) {
+	
+	if (isset($_POST["filter"])) $filter = @$_POST["filter"];
+	if (isset($_POST["filter_field"])) $filterfield = @$_POST["filter_field"];
+	$wholeonly = false;
+	if (isset($_POST["wholeonly"])) $wholeonly = @$_POST["wholeonly"];
+	
+	if (!isset($filter) && isset($_SESSION["filter"])) $filter = $_SESSION["filter"];
+	if (!isset($filterfield) && isset($_SESSION["filter_field"])) $filterfield = $_SESSION["filter_field"];
+
+	if (isset($filter)) $_SESSION["filter"] = $filter;
+	if (isset($filterfield)) $_SESSION["filter_field"] = $filterfield;
+	if (isset($wholeonly)) $_SESSION["wholeonly"] = $wholeonly;	
 	?><html>
 		<head>
 			<title>Manage Client</title>
@@ -22,6 +35,7 @@ if($_SESSION['validUser']) {
 			include("includes/header.php");?><br><?
 
 			$a = $_REQUEST["a"];
+			$b = $_REQUEST["b"];
 			$recid = $_REQUEST["recid"];
 			$sql = $_REQUEST["sql"]?$_REQUEST["sql"]:'';
 
@@ -86,29 +100,80 @@ if($_SESSION['validUser']) {
 
 			case "view":
 
+				$filter = "";
+				$filterfield = "";
+				$wholeonly = "";
+				$checkstr = "";
+				if ($wholeonly) $checkstr = " checked";
+
 				//Get FormCode
 				$formcode = $commonUses->getFormCode("Manage Client");
 				$access_file_level = $commonUses->checkFileAccess($_SESSION['staffcode'],$formcode);
 
-				$arrClient = $objCallData->sql_select();
+				$arrClient = $objCallData->sql_select($a,$recid);
 				$arrClientData = $arrClient[$recid];
 				$arrEmployees = $objCallData->fetchEmployees();
 				include('views/cli_client_view.php');
 				break;
 
 			case "edit":
-				$arrClient = $objCallData->sql_select();
+			
+				$filter = "";
+				$filterfield = "";
+				$wholeonly = "";
+				$checkstr = "";
+				if ($wholeonly) $checkstr = " checked";
+			
+				$arrClient = $objCallData->sql_select($a,$recid);
 				$arrClientData = $arrClient[$recid];
 				$arrEmployees = $objCallData->fetchEmployees();
 				include('views/cli_client_edit.php');
 				break;
-
+			
+			case "reset":
+			
+				//Get FormCode
+				$formcode = $commonUses->getFormCode("Manage Client");
+				$access_file_level = $commonUses->checkFileAccess($_SESSION['staffcode'],$formcode);
+				
+				// reset filter	
+				$filter = "";
+				$filterfield = "";
+				$wholeonly = "";
+				$checkstr = "";
+				if ($wholeonly) $checkstr = " checked";
+				 
+				//If View, Add, Edit, Delete all set to N
+				if($access_file_level == 0) {
+					echo "You are not authorised to view this file.";
+				}
+				else {
+					$arrClient = $objCallData->sql_select();
+					$arrEmployees = $objCallData->fetchEmployees();
+					include('views/cli_client_list.php');
+				}
+				
+				//header('location: cli_client.php');
+				break;
+				
 			default:
-
+			
+			
+			
 				//Get FormCode
 				$formcode = $commonUses->getFormCode("Manage Client");
 				$access_file_level = $commonUses->checkFileAccess($_SESSION['staffcode'],$formcode);
 
+				if($a == "reset") {
+					
+					$filter = "";
+					$filterfield = "";
+					$wholeonly = "";
+				}
+				
+				$checkstr = "";
+				if ($wholeonly) $checkstr = " checked";
+				 
 				//If View, Add, Edit, Delete all set to N
 				if($access_file_level == 0) {
 					echo "You are not authorised to view this file.";

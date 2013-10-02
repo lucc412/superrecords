abe5ab47f07849534380fc1dd61d57db04eacfbf
Disha <disha.goyal@befreeit.com.au>
2013-10-02 11:56:38 +0530

    remove unneccessary changes

--- admin/js/cli_validate.js
+++ admin/js/cli_validate.js
@@ -1,8 +1,17 @@
 function validateFormOnSubmit() { 
 	flagReturn = true;
 
+	
+
 	// do field validation  
-	if (document.manageclient.lstPractice.value == "") {
+	if (document.manageclient.dateSignedUp.value == "") {
+		alert("Enter Date Client Received");
+		document.manageclient.dateSignedUp.focus();
+		flagReturn = false;
+	}
+
+	// do field validation  
+	else if (document.manageclient.lstPractice.value == "") {
 		alert("Select Practice");
 		document.manageclient.lstPractice.focus();
 		flagReturn = false;
@@ -19,13 +28,6 @@ function validateFormOnSubmit() {
 	else if (document.manageclient.lstType.value == "") {
 		alert("Select Entity Type");
 		document.manageclient.lstType.focus();
-		flagReturn = false;
-	}
-
-	// do field validation  
-	else if (document.manageclient.dateSignedUp.value == "") {
-		alert("Enter Date Client Received");
-		document.manageclient.dateSignedUp.focus();
 		flagReturn = false;
 	}
 

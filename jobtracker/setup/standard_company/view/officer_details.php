47108b27caca9c2960f784775afaebe2ae337ed4
mailtosiddhesh <mailtosiddhesh@gmail.com>
2013-10-29 17:56:12 +0530

    Officer Bug

--- jobtracker/setup/standard_company/view/officer_details.php
+++ jobtracker/setup/standard_company/view/officer_details.php
@@ -26,7 +26,7 @@
                         <?php 
                             for($i = 1;$i <= 10;$i++) {
                                 $selectStr = '';
-                                if($StrAddState == $i) $selectStr = 'selected';
+                                if(count($arrOffcrData) == $i) $selectStr = 'selected';
                                 ?><option <?=$selectStr?> value="<?=$i?>"><?=$i?></option><?
                             }
                         ?>
@@ -38,61 +38,63 @@
             <?php
                 if(!empty($arrOffcrData))
                 {
+                     $cntr = 0;
                     foreach($arrOffcrData as $key => $value) 
                     { 
+                        $cntr++;
                         $noOfOffcr = $value['no_of_offcr'];
                     ?>
-                        <div id="officer_<?=$key?>"> 
-                            <div style="padding:10px 0;color: #F05729;font-size: 14px;">Officer <?=$key?>:</div>
-                            <input type="hidden" name="offcrId[<?=$key?>]" id="offcrId" value="<?=$key?>">
+                        <div id="officer_<?=$cntr?>"> 
+                            <div style="padding:10px 0;color: #F05729;font-size: 14px;">Officer <?=$cntr?>:</div>
+                            <input type="hidden" name="offcrId[<?=$cntr?>]" id="offcrId" value="<?=$cntr?>">
                             <table class="fieldtable">
                                 <tr>
                                     <td>First name </td>
-                                    <td><input type="text" id="txtFname_<?=$key?>" name="txtFname[<?=$key?>]" value="<?=$value['offcr_fname']?>" placeholder="First Name" /></td>
+                                    <td><input type="text" id="txtFname_<?=$cntr?>" name="txtFname[<?=$cntr?>]" value="<?=$value['offcr_fname']?>" placeholder="First Name" /></td>
                                 </tr>
                                 <tr>
                                     <td>Middle name </td>
-                                    <td><input type="text" id="txtMname_<?=$key?>" name="txtMname[<?=$key?>]" value="<?=$value['offcr_mname']?>" placeholder="Middle Name" /></td>
+                                    <td><input type="text" id="txtMname_<?=$cntr?>" name="txtMname[<?=$cntr?>]" value="<?=$value['offcr_mname']?>" placeholder="Middle Name" /></td>
                                 </tr>
                                 <tr>
                                     <td>Last name </td>
-                                    <td><input type="text" id="txtLname_<?=$key?>" name="txtLname[<?=$key?>]" value="<?=$value['offcr_lname']?>" placeholder="Last Name" /></td>
+                                    <td><input type="text" id="txtLname_<?=$cntr?>" name="txtLname[<?=$cntr?>]" value="<?=$value['offcr_lname']?>" placeholder="Last Name" /></td>
                                 </tr>
                                 <tr>
                                     <td>Date of birth </td>
-                                    <td><input type="text" id="txtDob_<?=$key?>" name="txtDob[<?=$key?>]" value="<?=date("d/m/Y",strtotime($value['offcr_dob']))?>" placeholder="Date of birth" readonly="" /><img src="../../images/calendar.png" id="calImgId" onclick="javascript:NewCssCal('txtDob_<?=$key?>','ddMMyyyy','dropdown',false,24,false,'past')" align="middle" class="calendar"/></td>
+                                    <td><input type="text" id="txtDob_<?=$cntr?>" name="txtDob[<?=$cntr?>]" value="<?=date("d/m/Y",strtotime($value['offcr_dob']))?>" placeholder="Date of birth" readonly="" /><img src="../../images/calendar.png" id="calImgId" onclick="javascript:NewCssCal('txtDob_<?=$cntr?>','ddMMyyyy','dropdown',false,24,false,'past')" align="middle" class="calendar"/></td>
                                 </tr>
                                 <tr>
                                     <td>City of birth </td>
-                                    <td><input type="text" id="txtCob_<?=$key?>" name="txtCob[<?=$key?>]" value="<?=$value['offcr_city_birth']?>" placeholder="City of birth" /><td>
+                                    <td><input type="text" id="txtCob_<?=$cntr?>" name="txtCob[<?=$cntr?>]" value="<?=$value['offcr_city_birth']?>" placeholder="City of birth" /><td>
                                 </tr>
                                 <tr>
                                     <td>State and Country of birth </td>
                                     <td>
-                                        <select id="selSob_<?=$key?>" name="selSob[<?=$key?>]" style="margin-bottom:5px; width:180px;" >
+                                        <select id="selSob_<?=$cntr?>" name="selSob[<?=$cntr?>]" style="margin-bottom:5px; width:180px;" >
                                             <option value="0">Select State</option><?php foreach($arrStates AS $stateKey => $stateName) { $selectStr = ""; if($value['offcr_state_birth'] == $stateKey) $selectStr = "selected"; ?><option <?=$selectStr?> value="<?=$stateKey?>"><?=$stateName?></option><? } ?>
                                         </select>
-                                        <select id="selCntryob_<?=$key?>" name="selCntryob[<?=$key?>]" style="margin-bottom:5px; width:180px;" >
+                                        <select id="selCntryob_<?=$cntr?>" name="selCntryob[<?=$cntr?>]" style="margin-bottom:5px; width:180px;" >
                                             <option value="0">Select Country</option><?php foreach($arrCountry AS $cntryKey => $cntryName) { $selectStr = ""; if($value['offcr_cntry_birth'] == $cntryKey) $selectStr = "selected"; ?><option <?=$selectStr?> value="<?=$cntryKey?>"><?=$cntryName?></option><? } ?>
                                         </select>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>Tax File Number </td>
-                                    <td><input type="text" id="txtTFN_<?=$key?>" name="txtTFN[<?=$key?>]" value="<?=$value['offcr_tfn']?>" placeholder="Tax File Number" /></td>
+                                    <td><input type="text" id="txtTFN_<?=$cntr?>" name="txtTFN[<?=$cntr?>]" value="<?=$value['offcr_tfn']?>" placeholder="Tax File Number" /></td>
                                 </tr>
                                 <tr>
                                     <td>Residential Address </td>
                                     <td>
                                         <div>
-                                            <input type="text" id="offAddUnit_<?=$key?>" name="offAddUnit[<?=$key?>]" style="width:115px;" value="<?=$value['offcr_addr_unit']?>" placeholder="Unit number" />
-                                            <input type="text" id="offAddBuild_<?=$key?>" name="offAddBuild[<?=$key?>]" style="width:115px;" value="<?=$value['offcr_addr_build']?>" placeholder="Building" />
-                                            <input type="text" id="offAddStreet_<?=$key?>" name="offAddStreet[<?=$key?>]" style="width:115px;" value="<?=$value['offcr_addr_street']?>" placeholder="Street"/><br>
-                                            <input type="text" id="offAddSubrb_<?=$key?>" name="offAddSubrb[<?=$key?>]" style="width:115px;" value="<?=$value['offcr_addr_subrb']?>" placeholder="Suburb"/>
-                                            <select id="offAddState_<?=$key?>" name="offAddState[<?=$key?>]" style="margin-bottom:5px; width:180px;" >
+                                            <input type="text" id="offAddUnit_<?=$cntr?>" name="offAddUnit[<?=$cntr?>]" style="width:115px;" value="<?=$value['offcr_addr_unit']?>" placeholder="Unit number" />
+                                            <input type="text" id="offAddBuild_<?=$cntr?>" name="offAddBuild[<?=$cntr?>]" style="width:115px;" value="<?=$value['offcr_addr_build']?>" placeholder="Building" />
+                                            <input type="text" id="offAddStreet_<?=$cntr?>" name="offAddStreet[<?=$cntr?>]" style="width:115px;" value="<?=$value['offcr_addr_street']?>" placeholder="Street"/><br>
+                                            <input type="text" id="offAddSubrb_<?=$cntr?>" name="offAddSubrb[<?=$cntr?>]" style="width:115px;" value="<?=$value['offcr_addr_subrb']?>" placeholder="Suburb"/>
+                                            <select id="offAddState_<?=$cntr?>" name="offAddState[<?=$cntr?>]" style="margin-bottom:5px; width:180px;" >
                                                 <option value="0">Select State</option><?php foreach($arrStates AS $stateKey => $stateName) { $selectStr = ""; if($value['offcr_addr_state'] == $stateKey) $selectStr = "selected"; ?><option <?=$selectStr?> value="<?=$stateKey?>"><?=$stateName?></option><? } ?>
                                             </select><br>
-                                            <input type="text" id="offAddPstCode_<?=$key?>" name="offAddPstCode[<?=$key?>]" value="<?=$value['offcr_addr_pst_code']?>" placeholder="Post Code"/>
+                                            <input type="text" id="offAddPstCode_<?=$cntr?>" name="offAddPstCode[<?=$cntr?>]" value="<?=$value['offcr_addr_pst_code']?>" placeholder="Post Code"/>
                                         </div>
                                     </td>
                                 </tr>

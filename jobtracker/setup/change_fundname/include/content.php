77946363e473693d6c1f5955d86d4e7401d52c7e
mailtosiddhesh <mailtosiddhesh@gmail.com>
2013-12-02 16:36:58 +0530

    Change fundname setup form completed

---
+++ jobtracker/setup/change_fundname/include/content.php
@@ -0,0 +1,7 @@
+<div class="introduction">
+    <ul>
+        <li>As members come and go from a SMSF or circumstances change the Trustee may seek to change the Fund’s name.</li>
+        <li>The rules of the relevant Trust or SMSF will detail the exact procedure to change the Fund name.</li>
+        <li>Failure to use this formula will see any proposed change ineffective so great care must be had to ensure the procedure is reviewed and implemented.</li>
+   </ul>    
+</div>
